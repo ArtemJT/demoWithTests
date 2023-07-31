41cda8145f0578d388fc2253103b72b4b294d6a4
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-07-31 11:55:46 +0300

    Added LiquiBase migration

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,6 @@
-create table public.users (
+create schema if not exists flyway;
+
+create table flyway.users (
                               id integer primary key not null default nextval('users_id_seq'::regclass),
                               name character varying,
                               email character varying,
@@ -7,14 +9,14 @@ create table public.users (
 );
 create unique index users_id_uindex on users using btree (id);
 
-create table public.addresses (
+create table flyway.addresses (
                                   id bigint primary key not null default nextval('addresses_id_seq'::regclass),
                                   address_has_active boolean,
                                   city character varying(255),
                                   country character varying(255),
                                   street character varying(255),
                                   employee_id integer,
-                                  foreign key (employee_id) references public.users (id)
+                                  foreign key (employee_id) references flyway.users (id)
                                       match simple on update no action on delete no action
 );
 
